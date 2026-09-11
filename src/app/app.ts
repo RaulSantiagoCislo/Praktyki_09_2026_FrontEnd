23c96b9a2fcd90a2034a2d0b374bfc5aa33a6629
DamianMa02 <damian.malawko@globallogic.com>
2026-09-11 00:47:42 -0700

    zapis na wszelik wypadek

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { Login } from './login/login';
 import { Zjezyki } from './zjezyki/zjezyki';
 import { Ustawienia } from './ustawienia/ustawienia';
+import { StronaG2 } from './StronaG2/StronaG2';
 
 @Component({
-  imports: [Login, Zjezyki, Ustawienia],
+  imports: [Login, Zjezyki, Ustawienia, StronaG2],
   selector: 'app-root',
   styleUrls: ['./app.css', './Wygląd.css'],
   templateUrl: './app.html',
@@ -12,7 +13,15 @@ import { Ustawienia } from './ustawienia/ustawienia';
 export class App {
   protected readonly title = signal('Demo');
 
+  // tekst powitania na stronie
+  Pokaz_TekstStronyGlownej = true;
 
+  // Strona G2
+  pokazStronaG2 = false;
+
+  pokazG2() {
+    this.pokazStronaG2 = true;
+  }
 
   // tutaj mi zamienia okna ustawień i jezyka by sie zamieniały
   pokazJezykiOkno = false;

9ef4ba349d73fec32e7f28b895fe5ecb965958f8
DamianMa02 <damian.malawko@globallogic.com>
2026-09-10 22:24:53 -0700

    może sie uda języki zamina

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Login } from './login/login';
 import { Zjezyki } from './zjezyki/zjezyki';
+import { Ustawienia } from './ustawienia/ustawienia';
 
 @Component({
-  imports: [Login, Zjezyki],
+  imports: [Login, Zjezyki, Ustawienia],
   selector: 'app-root',
   styleUrls: ['./app.css', './Wygląd.css'],
   templateUrl: './app.html',
@@ -11,9 +12,56 @@ import { Zjezyki } from './zjezyki/zjezyki';
 export class App {
   protected readonly title = signal('Demo');
 
+  // Okno wyboru języka
   pokazJezykiOkno = false;
 
   pokazJezyki() {
     this.pokazJezykiOkno = !this.pokazJezykiOkno;
+  }
+
+
+  pokazUstawieniaOkno = false;
+
+  pokazUstawienia() {
+    this.pokazUstawieniaOkno = !this.pokazUstawieniaOkno;
+  }
+
+
+  wybranyJezyk = 'Polski';
+
+
+  // PolskiJe = '';
+  // AngielskiJe = '';
+  // NiemieckiJe = '';
+  // FrancuskiJe =
+
+  zmienJezyk(jezyk: string) {
+    this.wybranyJezyk = jezyk;
+
+  //   if (jezyk === 'Polski') {
+  //     this.PolskiJe = 'Polski';
+  //     this.AngielskiJe = 'Polish';
+  //     this.NiemieckiJe = '';
+  //   }
+  //
+  //   if (jezyk === 'Angielski') {
+  //     this. = '';
+  //     this. = '';
+  //     this. = '';
+  //   }
+  //
+  //
+  //
+  //   if (jezyk === 'Niemiecki') {
+  //     this. = '';
+  //     this. = '';
+  //     this. = '';
+  //   }
+  //
+  //   if (jezyk === 'Francuski') {
+  //     this. = '';
+  //     this. = '';
+  //     this. = '';
+  //   }
   }
 }
--- src/app/zjezyki/zjezyki.ts
+++ src/app/zjezyki/zjezyki.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './zjezyki.css',
   templateUrl: './zjezyki.html',
 })
-export class Zjezyki {}
+export class Zjezyki {
+  @Output() zmianaJezyka = new EventEmitter<string>();
+
+  wybranyJezyk = 'Polski';
+
+  wybierzJezyk(jezyk: string) {
+    this.wybranyJezyk = jezyk;
+    this.zmianaJezyka.emit(jezyk);
+  }
+}
+
+

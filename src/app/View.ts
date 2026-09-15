ce288dd83b9bfed9a38a230bc5f09cbbb864a93d
Raul Cislo <raul.cislo@globallogic.com>
2026-09-15 10:44:38 +0200

    ixed login and register

--- src/app/View.ts
+++ src/app/View.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [], // tu przecink jest wazny jak cos 
   templateUrl: './View.html',
   styleUrl: './View.css',
 })
 export class Login {
-  // UWAGA: Zmienna i funkcja muszą być DOKŁADNIE tutaj, między nawiasami { }
-  // pokazFormularz = false;
-  //kliknijPrzycisk() {
-  //   this.pokazFormularz = !this.pokazFormularz;
-  // }
+  http = inject(HttpClient);
+
+  zaloguj(emailWpisane: string, hasloWpisane: string) {
+    const daneDoLogowania = {
+      email: emailWpisane,
+      password: hasloWpisane 
+    };
+
+    // tutaj musimy zmienic ip na na ip backendu 
+    this.http.post('http://192.168.X.X:5015/api/login', daneDoLogowania).subscribe({
+      next: (odpowiedz) => {
+        console.log(odpowiedz);
+      },
+      error: (blad) => {
+        console.error(blad);
+      }
+    });
+  }
 }

e6fc3f37d84c4246e9b8b8616189fc69cd7ccd02
Raul Cislo <raul.cislo@globallogic.com>
2026-09-16 16:59:46 +0200

    refactor: reorganizacja struktury w folderze src i usuniecie zbednych plikow

--- src/app/Rest_of_Website/Travel/Travel.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-Travel',
-  styleUrl: './Travel.css',
-  templateUrl: './Travel.html',
-})
-export class Travel {}

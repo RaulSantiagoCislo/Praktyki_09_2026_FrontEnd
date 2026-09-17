205c20b8932301d83d2defce3adff359e1375cac
RaulSantiagoCislo <raul.cislo@globallogic.com>
2026-09-17 18:15:29 +0200

    refactor: reorganizacja struktury w folderze src i usuniecie zbednych plikow (#9)

--- src/app/Rest_of_Website/Culture/Culture.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-culture',
-  styleUrl: './Culture.css',
-  templateUrl: './Culture.html',
-})
-export class Culture {}

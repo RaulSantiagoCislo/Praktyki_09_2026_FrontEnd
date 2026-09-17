0244487f60c6d678a0f3b74c22caa11b7a155915
Grzegorz Gertruda <grzegorz.gertruda@globallogic.com>
2026-09-17 13:29:40 +0200

    Dodano panel administratora i zaktualizowano pliki aplikacji

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, Routes } from '@angular/router';
+import { AdminPanel } from './Components/admin-panel/admin-panel';
+
+const routes: Routes = [
+  { path: 'admin-panel', component: AdminPanel },
+  { path: '**', redirectTo: '' },
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient()],
+  providers: [provideBrowserGlobalErrorListeners(), provideHttpClient(), provideRouter(routes)],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './Components/login/login.component';
 import { Ch1Language } from './Components/Ch1Language/Ch1Language';
 import { Settings } from './Components/Settings/Settings';
 import { RegisterComponent } from './Components/register/register.component';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [LoginComponent, Ch1Language, Settings, RegisterComponent],
+  imports: [LoginComponent, Ch1Language, Settings, RegisterComponent, RouterLink, RouterOutlet],
   selector: 'app-root',
   styleUrls: ['./app.css'],
   templateUrl: './app.html',
@@ -40,4 +41,6 @@ export class App {
   ChangeLanguage(language: string) {
     this.ChosenLanguage = language;
   }
+
+  constructor(public router: Router) {}
 }

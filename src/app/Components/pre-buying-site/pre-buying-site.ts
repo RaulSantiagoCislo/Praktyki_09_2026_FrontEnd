7ef02e5d60b093f3dfd9f5e982bd7556584bd667
DamianMa02 <damian.malawko@globallogic.com>
2026-09-17 05:12:12 -0700

    using events instead of same templates  (also I didn't remove css in styles css because it kinda destroyes how site looks

--- src/app/Components/pre-buying-site/pre-buying-site.ts
+++ src/app/Components/pre-buying-site/pre-buying-site.ts
@@ -4,10 +4,19 @@ import { Router } from '@angular/router';
 @Component({
   imports: [],
   selector: 'app-pre-buying-site',
-  styleUrl: './pre-buying-site.css',
+  // styleUrl: './',
   templateUrl: './pre-buying-site.html',
 })
 export class PreBuyingSite {
+  eventInformation = { id: 1, name: 'a', date: 'a', location: 'a', organizer: 'a' };
+
+  tickets = [
+    { id: 1, date: 'a', time: '8:00 - 9:00' },
+    { id: 2, date: 'a', time: '8:00 - 9:00' },
+    { id: 3, date: 'a', time: '8:00 - 9:00' },
+    { id: 4, date: 'a', time: '8:00 - 9:00' },
+  ];
+
   constructor(private router: Router) {}
 
   Exit() {
--- src/app/Components/searching-results/searching-results.ts
+++ src/app/Components/searching-results/searching-results.ts
@@ -2,9 +2,52 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [RouterLink],
-  selector: 'app-searching-results',
-  styleUrl: './searching-results.css',
-  templateUrl: './searching-results.html',
+imports: [RouterLink],
+selector: 'app-searching-results',
+styleUrl: './searching-results.css',
+templateUrl: './searching-results.html',
 })
-export class SearchingResults {}
+export class SearchingResults {
+eventInformation = [
+{
+id: 1,
+category: 'Kategoria',
+name: 'Nazwa',
+date: '17.09.2026 - 19.09.2026',
+time: '8:00 - 9:00',
+location: 'Warszawa',
+},
+{
+id: 2,
+category: 'Kategoria',
+name: 'Nazwa',
+date: '17.09.2026 - 19.09.2026',
+time: '8:00 - 9:00',
+location: 'Warszawa',
+},
+{
+id: 3,
+category: 'Kategoria',
+name: 'Nazwa',
+date: '17.09.2026 - 19.09.2026',
+time: '8:00 - 9:00',
+location: 'Warszawa',
+},
+{
+id: 4,
+category: 'Kategoria',
+name: 'Nazwa',
+date: '17.09.2026 - 19.09.2026',
+time: '8:00 - 9:00',
+location: 'Warszawa',
+},
+{
+id: 5,
+category: 'Kategoria',
+name: 'Nazwa',
+date: '17.09.2026 - 19.09.2026',
+time: '8:00 - 9:00',
+location: 'Warszawa',
+},
+];
+}
